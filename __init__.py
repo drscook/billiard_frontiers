adf8c7ca3f718148c925d8568eecacf4416e1df1
Cook <SCOOK@tarleton.edu>
2018-09-08 14:50:19 -0500

    fdsf

--- __init__.py
+++ __init__.py
@@ -10,4 +10,4 @@ import matplotlib.pyplot as plt
 
 np.set_printoptions(precision=4, suppress=True)
 
-import billiard.util.utility_functions as util
+import billiards.util.utility_functions as util
