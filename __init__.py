3f50ba676697921fa36caf0fa6460ee74d140d1a
Cook <SCOOK@tarleton.edu>
2018-09-08 18:22:16 -0500

    gfgf

--- __init__.py
+++ __init__.py
@@ -34,7 +34,7 @@ pd.options.display.show_dimensions = True
 pd.options.display.max_rows = 20
 pd.options.display.max_columns = None
 
-def display(X, max_rows=20, max_columns=None):
+def display(X, max_rows=20, max_cols=None):
     with pd.option_context('display.max_rows', max_rows, 'display.max_columns', max_columns):
         if isinstance(X, pd.Series) or (isinstance(X, np.ndarray) and X.ndim <=2):
             IPython.display.display(pd.DataFrame(X))
